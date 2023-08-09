c536ede5835fae2cb359ba7756d83ba42b4f17d6
Alina Havriushenko <alinagvr@gmail.com>
2023-08-09 18:33:52 +0200

    add admin hook

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -6,3 +6,13 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
+
+function admin_link($items, $args) {
+    if  (is_user_logged_in() && $args->menu) {
+$admin_menu_item = '<li><a href="#">Admin</a></li>';
+        $items = $admin_menu_item . $items;
+    }
+    return $items;
+};
+
+add_filter( 'wp_nav_menu_items', 'admin_link', 10, 2 );
